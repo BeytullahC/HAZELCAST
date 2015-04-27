2252268173545e749b9cddce2bce9f28c7fcb0bc
BeytullahC <beytullah.dakich@gmail.com>
2015-04-27 17:33:40 +0300

    ECLIPSELINK CONF. AND HR DB SQL ADDED.

--- HAZELCAST-CORE/src/test/java/com/beto/project/hazelcast/core/Tester.java
+++ HAZELCAST-CORE/src/test/java/com/beto/project/hazelcast/core/Tester.java
@@ -11,69 +11,87 @@ import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
  * @author 912867
  */
 public class Tester {
+    static Config cfg = new Config();
+    static HazelcastInstance instance;
+    static ClientConfig clientConfig;
+    static HazelcastInstance client;
+    static Queue<String> queueCustomers;
+    static Map<Integer, String> mapCustomers;
+    static IMap map;
+
+
+    @BeforeClass
+    public static void testInit() {
+        System.out.println("BEFORE RUNNING");
+        instance = Hazelcast.newHazelcastInstance(cfg);
+        clientConfig = new ClientConfig();
+        client = HazelcastClient.newHazelcastClient(clientConfig);
+    }
+
+    @AfterClass
+    public static void destroy() {
+        System.out.println("AFTER RUNNING");
+        queueCustomers.clear();
+        mapCustomers.clear();
+        client.shutdown();
+    }
 
     @Test
-    public void test() throws InterruptedException {
+    public void addDataToQueue() throws InterruptedException {
         @SuppressWarnings("deprecation")
-        ClientConfig clientConfig = new ClientConfig();
-        HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
         BlockingQueue<String> queue = client.getQueue("queue");
         queue.put("Hello!");
         System.out.println("Message send by client!");
         assertTrue(queue.contains("Hello!"));
-        client.shutdown();
-
     }
 
     @Test
     public void addDataToMap() {
-        Config cfg = new Config();
-        HazelcastInstance instance = Hazelcast.newHazelcastInstance(cfg);
-        Map<Integer, String> mapCustomers = instance.getMap("customers");
+        mapCustomers = instance.getMap("customers");
         mapCustomers.clear();
         mapCustomers.put(1, "Joe");
         mapCustomers.put(2, "Ali");
         mapCustomers.put(3, "Avi");
-
-        System.out.println("Customer with key 1: " + mapCustomers.get(1));
-        System.out.println("Map Size:" + mapCustomers.size());
-        assertEquals(mapCustomers.get(1), "Joe");
-        assertNotEquals(mapCustomers.get(3), "Joe");
-        Queue<String> queueCustomers = instance.getQueue("customers");
+        queueCustomers = instance.getQueue("customers");
         queueCustomers.clear();
         queueCustomers.offer("Tom");
         queueCustomers.offer("Mary");
         queueCustomers.offer("Jane");
-        assertEquals(3, queueCustomers.size());
-        System.out.println("First customer: " + queueCustomers.poll());
-        System.out.println("Second customer: " + queueCustomers.peek());
-        mapCustomers.clear();
-        queueCustomers.remove();
-        instance.shutdown();
     }
 
     @Test
-    public void getData(){
-        ClientConfig clientConfig = new ClientConfig();
-        HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
-        IMap map = client.getMap("customers");
-
+    public void getDataToMap() {
+        map = client.getMap("customers");
         System.out.println("Map Size:" + map.size());
-        map.destroy();
-        client.shutdown();
+        System.out.println("Customer with key 1: " + map.get(1));
+        System.out.println("Map Size:" + map.size());
+        assertEquals(map.get(1), "Joe");
+        assertNotEquals(map.get(3), "Joe");
+    }
+
+    @Test
+    public void getDataToQueue() {
+        System.out.println("First customer: " + queueCustomers.poll());
+        System.out.println("Second customer: " + queueCustomers.peek());
+        System.out.println(queueCustomers.size());
+        assertEquals(2, queueCustomers.size());
+        assertEquals("Mary", queueCustomers.poll());
+        assertEquals("Jane", queueCustomers.poll());
+        System.out.println(queueCustomers.size());
     }
 }
---
+++ HAZELCAST-ECLIPSELINK/src/main/java/com/beto/project/hazelcast/jpa/entities/Parameters.java
@@ -0,0 +1,9 @@
+package com.beto.project.hazelcast.jpa.entities;
+
+/**
+ * Created by 912867 on 27.04.2015.
+ */
+public class Parameters {
+
+    public static final String TEST_PERSISTANCE_UNIT = "HR_TEST_UNIT";
+}
