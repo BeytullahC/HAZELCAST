c3d2ccab62e49cef29a65ce3664d284011fa4f9a
BeytullahC <beytullah.dakich@gmail.com>
2015-04-28 22:21:19 +0300

    EJB and Jpa test completed
    Glassfish 4.1 OUTPUT :
    
    [2015-04-28T22:16:00.015+0300] [glassfish 4.1] [INFO] [] [] [tid: _ThreadID=145 _ThreadName=Thread-8] [timeMillis: 1430248560015] [levelValue: 800] [[
      SIZE : 25]]

---
+++ HAZELCAST-EJB-JPA/src/main/java/model/entities/Departments.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author 912867
+ */
+@Entity
+@Table(name = "departments")
+@XmlRootElement
+@NamedQueries({
+        @NamedQuery(name = "Departments.findAll", query = "SELECT d FROM Departments d"),
+        @NamedQuery(name = "Departments.findByDepartmentId", query = "SELECT d FROM Departments d WHERE d.departmentId = :departmentId"),
+        @NamedQuery(name = "Departments.findByDepartmentName", query = "SELECT d FROM Departments d WHERE d.departmentName = :departmentName")})
+public class Departments implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "department_id")
+    private Integer departmentId;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 30)
+    @Column(name = "department_name")
+    private String departmentName;
+    @JoinColumn(name = "location_id", referencedColumnName = "location_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private model.entities.Locations locationId;
+    @JoinColumn(name = "manager_id", referencedColumnName = "employee_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Employees managerId;
+    @OneToMany(mappedBy = "departmentId", fetch = FetchType.LAZY)
+    private List<Employees> employeesList;
+
+    public Departments() {
+    }
+
+    public Departments(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Departments(Integer departmentId, String departmentName) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public model.entities.Locations getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(Locations locationId) {
+        this.locationId = locationId;
+    }
+
+    public Employees getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Employees managerId) {
+        this.managerId = managerId;
+    }
+
+    @XmlTransient
+    public List<Employees> getEmployeesList() {
+        return employeesList;
+    }
+
+    public void setEmployeesList(List<Employees> employeesList) {
+        this.employeesList = employeesList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (departmentId != null ? departmentId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof model.entities.Departments)) {
+            return false;
+        }
+        model.entities.Departments other = (model.entities.Departments) object;
+        if ((this.departmentId == null && other.departmentId != null) || (this.departmentId != null && !this.departmentId.equals(other.departmentId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.beto.project.hazelcast.jpa.com.beto.project.hazelcast.jpa.model.Departments[ departmentId=" + departmentId + " ]";
+    }
+
+}
--- HAZELCAST-EJB-JPA/src/main/java/test/EjbTester.java
+++ HAZELCAST-EJB-JPA/src/main/java/test/EjbTester.java
@@ -1,11 +1,8 @@
 package test;
 
 import dao.local.CountriesFacadeLocal;
-import javax.ejb.EJB;
-import javax.ejb.LocalBean;
-import javax.ejb.Schedule;
-import javax.ejb.Singleton;
-import javax.ejb.Timer;
+
+import javax.ejb.*;
 
 /**
  * Created by 912867 on 28.04.2015.
@@ -14,8 +11,10 @@ import javax.ejb.Timer;
 @LocalBean
 @Singleton
 public class EjbTester {
+
     @EJB
     private CountriesFacadeLocal local;
+
     @Schedule(second = "00",minute = "*",hour = "*")
     public void Test(Timer t){
         System.out.println("SIZE : "+local.findAll().size());
